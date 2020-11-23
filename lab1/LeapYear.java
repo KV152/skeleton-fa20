32dddd24ca51173c488aafbba3c971ee8fb9e15d
alf <alfshi3@gmail.com>
2020-11-23 13:50:09 +0800

    completed LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -13,6 +13,14 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+     
+    /** A leap year is either: divisible by 400 or divisible by 4 and not by 100. **/ 
+    private static boolean isLeapYear(int year) {
+    	if (year%400 == 0 || (year%4==0 && !(year%100==0)))
+		return true;
+	else
+		return false;
+    }
 
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
